b84f9bd5e5e276546d7716d4a504d81d5ddc7eec
Jorge Servin <jservingo@gmail.com>
2026-01-28 23:09:35 -0400

    Primer commit

---
+++ app/src/main/java/com/krental/cursodejetpackcompose/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.krental.cursodejetpackcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFE91E63)
+val PurpleGrey80 = Color(0xFFFF6090)
+val Pink80 = Color(0xFFFF90AA)
+
+val Purple40 = Color(0xFFE91E63)
+val PurpleGrey40 = Color(0xFFFF6090)
+val Pink40 = Color(0xFFFF90AA)
+
+
+//Colores más claros
+val Blue80 = Color(0xFF00FFFF)
+val BlueGrey80 = Color(0xFF00AAFF)
+val BluePink80 = Color(0xFF90AAFF)
+
+//Colores más oscuros
+val Blue40 = Color(0xFF0000FF)
+val BlueGrey40 = Color(0xFF0060FF)
+val BluePink40 = Color(0xFF4090FF)
---
+++ app/src/main/java/com/krental/cursodejetpackcompose/ui/theme/Theme.kt
@@ -0,0 +1,64 @@
+package com.krental.cursodejetpackcompose.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+//En modo oscuro los colores deben ser más claros
+private val DarkColorScheme = darkColorScheme(
+    primary = Blue80,
+    secondary = BlueGrey80,
+    tertiary = BluePink80
+)
+
+//En modo claro los colores deben ser más oscuros
+private val LightColorScheme = lightColorScheme(
+    primary = Blue40,
+    secondary = BlueGrey40,
+    tertiary = BluePink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun CursoDeJetpackComposeTheme(
+    darkTheme: Boolean = false, //isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        //Si dynamic color esta en true y la version de android es mayor o igual a 12
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme)
+                dynamicDarkColorScheme(context)
+            else
+                dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
